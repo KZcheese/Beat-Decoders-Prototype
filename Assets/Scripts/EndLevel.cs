85791e4d0592a1bdca166ec8347ba9f6dc9343a5
Kevin Zhan <kevin.w.zhan@gmail.com>
2025-01-08 13:04:55 -0500

    level 1 (not tutorial) done

--- Assets/Scripts/EndLevel.cs
+++ Assets/Scripts/EndLevel.cs
@@ -5,17 +5,9 @@ using UnityEngine.SceneManagement;
 public class EndLevel : MonoBehaviour
 {
     public string nextLevel;
-    private bool _visible;
-
-    private void Start()
-    {
-        gameObject.SetActive(_visible);
-    }
-
     public void ShowScreen(bool visible)
     {
-        _visible = visible;
-        gameObject.SetActive(_visible);
+        gameObject.SetActive(visible);
     }
 
     public void NextLevel()
